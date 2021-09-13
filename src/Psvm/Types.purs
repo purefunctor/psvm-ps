b575fbd9b65e6002927c24a37c755ebf797ae391
PureFunctor <purefunctor@gmail.com>
2021-09-14 00:59:39 +0800

    Move Aff and Effect from runFailure into runPsvm
    This change encourages purity in the runFailure function, allowing
    possible consumers of this particular effect to supply their own
    error-handling routines as demonstrated in runPsvm.

--- src/Psvm/Types.purs
+++ src/Psvm/Types.purs
@@ -4,10 +4,11 @@ import Prelude
 
 import Data.Either (Either(..), either)
 import Effect.Aff (Aff, attempt)
-import Effect.Console as Console
+import Effect.Class as EFfect
+import Effect.Class.Console as Console
 import Effect.Exception (Error, message)
 import Node.Process as Process
-import Run (AFF, EFFECT, Run, liftAff, liftEffect, runBaseAff')
+import Run (AFF, EFFECT, Run, liftAff, runBaseAff')
 import Run.Except (EXCEPT, Except(..), liftExcept, runExcept)
 import Run.Reader (READER, runReader)
 import Type.Row (type (+))
@@ -25,16 +26,9 @@ exit c m = liftExcept $ Except (Exit c m)
 exitError :: forall a. Int -> Error -> Psvm a
 exitError c = exit c <<< message
 
--- | Raise a `Failure` into `Effect`.
-runFailure :: forall r a. Run (EXCEPT Failure + EFFECT + r) a -> Run (EFFECT + r) a
-runFailure m = do
-  u <- runExcept m
-  case u of
-    Left f -> case f of
-      Exit code message -> liftEffect do
-        Console.error message
-        Process.exit code
-    Right r -> pure r
+-- | Run an optionally-failing action.
+runFailure :: forall r a. Run (EXCEPT Failure + r) a -> Run r (Either Failure a)
+runFailure = runExcept
 
 -- | Environment type consumed by the app.
 type Env = {}
@@ -53,7 +47,16 @@ type Psvm a = Run PSVM a
 
 -- | Run the `PSVM` effect stack.
 runPsvm :: forall a. Env -> Psvm a -> Aff a
-runPsvm e = runBaseAff' <<< runFailure <<< runReader e
+runPsvm e = runFailureBaseAff <<< runFailure <<< runReader e
+  where
+  runFailureBaseAff m = do
+    u <- runBaseAff' m
+    case u of
+      Left f -> case f of
+        Exit code message -> EFfect.liftEffect do
+          Console.error message
+          Process.exit code
+      Right r -> pure r
 
 -- | Lift and catch `Aff` action errors in `Psvm`.
 liftCatchAff :: forall r. Aff r -> (Error -> Psvm r) -> Psvm r
